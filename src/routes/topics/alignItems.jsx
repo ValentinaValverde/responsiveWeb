db208317dec83bde2476147258ab6787af08ab31
Valentina Valverde <vvgreenville@gmail.com>
2023-10-08 18:57:46 -0400

    Finished align-items page.

--- src/routes/topics/alignItems.jsx
+++ src/routes/topics/alignItems.jsx
@@ -1,9 +1,40 @@
 import { Link } from "react-router-dom"
 
 export default function AlignItems() {
-    return(
+    return (
         <>
-            <h2>Align Items</h2>
+            <div className="directionMain">
+                <h2>Align Items</h2>
+                <p>is used to align the flex items</p>
+            </div>
+
+            <div className="alignItemsContainer">
+                <h3>CENTER</h3>
+                <div className="center">
+                    <p className="pTag">aligns the flex items in the middle of the container</p>
+                </div>
+
+                <h3>FLEX-START</h3>
+                <div className="flexStart">
+                    <p className="pTag">aligns the flex items at the top of the container</p>
+                </div>
+
+                <h3>FLEX-END</h3>
+                <div className="flexEnd">
+                    <p className="pTag">aligns the flex items at the bottom of the container</p>
+                </div>
+
+                <h3>STRETCH</h3>
+                <div className="stretch">
+                    <p className="pTag">stretches the flex items to fill the container (this is default)</p>
+                </div>
+
+                <h3>BASELINE</h3>
+                <div className="baseline">
+                    <p className="pTag">aligns the flex items such as their baselines aligns</p>
+                    <p className="pTag">aligns the flex items such as their baselines aligns</p>
+                </div>
+            </div>
 
 
 
