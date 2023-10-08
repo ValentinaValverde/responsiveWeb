2ca7042e2446c18c705b7846659130d583f62937
Valentina Valverde <vvgreenville@gmail.com>
2023-10-07 21:04:04 -0400

    Finished flex-direction page.

--- src/routes/topics/direction.jsx
+++ src/routes/topics/direction.jsx
@@ -1,10 +1,43 @@
-
-
+import { Link } from "react-router-dom"
 
 export default function FlexDirection() {
     return(
         <>
+        <div className="directionMain">
             <h2>Flex Direction</h2>
+            <p>The flex-direction property defines in which direction the container wants to stack the flex items.</p>
+        </div>
+
+
+            <h3>ROW</h3>
+            <div className="rowContainer">
+                <div className="rowP">
+                    <h3>row</h3>
+                    <p>stacks the flex items horizontally (from left to right)</p>
+                </div>
+
+                <div className="rowP">
+                    <h3>row-reverse</h3>
+                    <p>stacks the flex items horizontally (but from right to left)</p>
+                </div>
+            </div>
+
+            <h3>COLUMN</h3>
+            <div className="columnContainer">
+                <div className="columnP">
+                    <h3>column</h3>
+                    <p>stacks the flex items vertically (from top to bottom)</p>
+                </div>
+
+                <div className="columnP">
+                    <h3>column-reverse</h3>
+                    <p>stacks the flex items vertically (but from bottom to top)</p>
+                </div>
+            </div>
+            <div className="nextTopic1">
+                <Link className="next" to={"/flexWrap"}>next topic</Link>
+            </div>
+
         </>
     )
 }
--- src/routes/topics/wrap.jsx
+++ src/routes/topics/wrap.jsx
@@ -2,7 +2,29 @@
 export default function FlexWrap() {
     return(
         <>
+        <div className="directionMain">
             <h2>Flex Wrap</h2>
+            <p>The flex-wrap property specifies whether the flex items should wrap or not.</p>
+        </div>        
+        
+        <div className="containerWrap">
+            <div className="wrapP">
+                <p>container 1</p>
+            </div>
+            <div className="warpP">
+                <p>container 2</p>
+            </div>
+        </div>
+
+        <div className="containerNoWrap">
+        <div className="noWrapP">
+                <p>container 1</p>
+            </div>
+            <div className="noWarpP">
+                <p>container 2</p>
+            </div>
+        </div>
+        
         </>
     )
 }
